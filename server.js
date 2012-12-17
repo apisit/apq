ee05a1e47a0c9f8041b0518e9a30612a6d942f9e
apisit <apmotive@gmail.com>
2012-12-16 19:50:50 -0800

    change tag to {alt}

--- server.js
+++ server.js
@@ -44,11 +44,12 @@ opHelper.execute('ItemSearch', {
 //var count =results.ItemSearchResponse.Items[0].TotalResults;
 
 //console.log(count);
-var itemTemplate = "<div class=\"box photo\" title=\"{text}\"><a  href=\"http://www.amazon.com/dp/{asin}?tag=apmo0d-20\"><img src=\"{image}\"/><p>{text}</p></a></div>";
+var itemTemplate = "<div class=\"box photo\" title=\"{alt}\"><a  href=\"http://www.amazon.com/dp/{asin}?tag=apmo0d-20\"><img src=\"{image}\"/><p>{text}</p></a></div>";
  for(i =0;i<10;i++){
 	//htmlResult+=results.ItemSearchResponse.Items[0].Item[i].ASIN +"<br/>";
 
  	htmlResult+=itemTemplate.replace("{text}", results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title);
+ 	htmlResult=htmlResult.replace("{alt}", results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title);
  	htmlResult=htmlResult.replace("{asin}", results.ItemSearchResponse.Items[0].Item[i].ASIN);
   if (results.ItemSearchResponse.Items[0].Item[i].LargeImage!=null){
 
