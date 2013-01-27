824de4d6eceec602108e54223a2d554c2ad09cf9
apisit <apmotive@gmail.com>
2013-01-27 10:04:52 -0800

    update

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+
+app.get('/',function(req,response){
+  var fs=require('fs');
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+   
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+
 });
 
-
+});
 
 
 
@@ -27,59 +34,84 @@ var opHelper = new OperationHelper({
     assocId:   'apmo0d-20',
 });
 var searchtime =  Date.now();
-console.log(searchtime  + " " + req.params.query);
+console.log(searchtime  + " "+ req.params.searchIndex + " " + req.params.query + " " + req.query["p"]);
 
 opHelper.execute('ItemSearch', {
     'SearchIndex': req.params.searchIndex,
-    'Keywords': req.params.query,
-    'ResponseGroup': 'Large',
+    'Keywords': unescape(req.params.query),
+    'ResponseGroup': 'Medium',
     'ItemPage':req.query["p"]==null?1:req.query["p"]
 }, function(error, results) {
-    if (error) { sys.print('Error: ' + error + "\n") }
+    if (error) { 
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Content-Length', "".length);
+      res.end("");
+     }
+
     var htmlResult="";
-  //  sys.print("Results:\n" + results.ItemSearchResponse.Items[0] + "\n");
-// console.log(results.ItemSearchResponse.Items[0].TotalPages);
-// console.log(results.ItemSearchResponse.Items[0].MoreSearchResultsUrl);
 
- var count =results.ItemSearchResponse.Items[0].TotalResults;
-//console.log(count);
+    var totalPage=parseInt(results.ItemSearchResponse.Items[0].TotalPages);
+    //console.log(totalPage);
+    //if totalpage < given page then return blank
+    var requestPage=parseInt(req.query["p"]);
+    if (requestPage>totalPage){
+      //console.log("error requestPage>totalPage");
+      console.log("%j",results.ItemSearchResponse.Items[0].Request[0].Errors[0].Error[0].Message);
+      htmlResult="";
+    }else{
+      var count =parseInt(results.ItemSearchResponse.Items[0].TotalResults);
+      //console.log(count);
 
-var itemTemplate = "<div class=\"box photo\" title=\"{alt}\"><a  href=\"http://www.amazon.com/dp/{asin}?tag=apmo0d-20\"><img src=\"{image}\"/><p>{text}</p></a></div>";
-if (count>0){
+      var itemTemplate = "<div class=\"box photo\" title=\"{alt}\"><a href=\"http://www.amazon.com/dp/{asin}?tag=apmo0d-20\"><img src=\"{image}\"><p>{text}</p></a></div>";
+      if (count==0){
+          console.log("no result" + count);
+         htmlResult= results.ItemSearchResponse.Items[0].Request[0].Errors[0].Error[0].Message;
+      }else{
+           if (count>10)
+            count=10;
 
+          for(i =0;i<count;i++){
+            //console.log(i);
+           
+            if (results.ItemSearchResponse.Items[0].Item[i]!=null){
+               htmlResult+=itemTemplate;
+               //console.log(results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title);
+               var title  = results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title[0];
+               title=escape(title);
+               //console.log(title);
+                htmlResult=htmlResult.replace("{text}", title);
+                htmlResult=htmlResult.replace("{alt}", title);
+                htmlResult=htmlResult.replace("{asin}", results.ItemSearchResponse.Items[0].Item[i].ASIN);
 
-for(i =0;i<10;i++){
-	//htmlResult+=results.ItemSearchResponse.Items[0].Item[i].ASIN +"<br/>";
- 	htmlResult+=itemTemplate.replace("{text}", results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title);
- 	htmlResult=htmlResult.replace("{alt}", results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title);
- 	htmlResult=htmlResult.replace("{asin}", results.ItemSearchResponse.Items[0].Item[i].ASIN);
-  if (results.ItemSearchResponse.Items[0].Item[i].LargeImage!=null){
+                if (results.ItemSearchResponse.Items[0].Item[i].LargeImage!=null){
+              
+                    htmlResult=htmlResult.replace("{image}", results.ItemSearchResponse.Items[0].Item[i].LargeImage[0].URL);
+                    //console.log(results.ItemSearchResponse.Items[0].Item[i].LargeImage[0].URL[0]);
+                }else{
+                    console.log("no image");
+                    htmlResult=htmlResult.replace("{image}","http://placehold.it/200x200");
+                    //console.log("something wrong : %j", results.ItemSearchResponse.Items[0].Item[i]);
+                }
+                //console.log(htmlResult + "\n\n");
 
-	htmlResult=htmlResult.replace("{image}", results.ItemSearchResponse.Items[0].Item[i].LargeImage[0].URL);
-  }
-	// var price;
- // 	if (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice!=null)
- // 	price= results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice[0].FormattedPrice;
-	// else
-	// price =results.ItemSearchResponse.Items[0].Item[i].OfferSummary[0].LowestNewPrice[0].FormattedPrice;
-	// htmlResult+=price +"<br/>";
- 	//htmlResult+=results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice;
- }//end for loop
- }else{
-htmlResult= results.ItemSearchResponse.Items[0].Request[0].Errors[0].Error[0].Message;
+            }
+           
+           
+            
+            // var price;
+           //   if (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice!=null)
+           //   price= results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice[0].FormattedPrice;
+            // else
+            // price =results.ItemSearchResponse.Items[0].Item[i].OfferSummary[0].LowestNewPrice[0].FormattedPrice;
+            // htmlResult+=price +"<br/>";
+            //htmlResult+=results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice;
+           }//end for loop
+      }//end else count >0
 
- }
-
-fs.readFile('template.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
- htmlResult=data.replace("{product}",htmlResult);
- htmlResult=htmlResult.replace("{title}",req.params.query);
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', htmlResult.length);
+    }
+   res.setHeader('Content-Type', 'text/html');
+   res.setHeader('Content-Length', htmlResult.length);
    res.end(htmlResult);
-});
 
 	
 });
